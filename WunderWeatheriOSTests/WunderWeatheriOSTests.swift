367c35b49c1464ea9a7362b4fcc558f272290599
Nikhil Tanappagol <nikhiltanappagol@Nikhils-MacBook-Pro.local>
2017-09-18 20:40:39 -0700

    Initial Commit

---
+++ WunderWeatheriOSTests/WunderWeatheriOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  WunderWeatheriOSTests.swift
+//  WunderWeatheriOSTests
+//
+//  Created by Nikhil Tanappagol on 9/18/17.
+//  Copyright © 2017 Nikhil Patil. All rights reserved.
+//
+
+import XCTest
+@testable import WunderWeatheriOS
+
+class WunderWeatheriOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
